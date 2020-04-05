0a376e92ab1af5254a4abf202a56b280bb2007a9
huqinghua109 <huqinghua109@163.com>
2020-04-05 20:45:41 +0800

    debug vol

--- Digiccy1/maker_arb/maker_algo.py
+++ Digiccy1/maker_arb/maker_algo.py
@@ -370,7 +370,7 @@ class SpreadMakerAlgo(SpreadAlgoTemplate):
             if self.submitting_long_dict['order_id'] is None:
                 # 可用资金不足，调整数量
                 if shadow_bid * vol > self.algo_engine.margin_accounts["USDTUSDT."+self.active_leg.exchange.value].free:
-                    vol = self.algo_engine.margin_accounts["USDTUSDT."+self.active_leg.exchange.value].free * 0.9
+                    vol = self.algo_engine.margin_accounts["USDTUSDT."+self.active_leg.exchange.value].free * 0.9 / shadow_bid
                 # 不足最小金额，立即返回
                 if shadow_bid * vol < 12:
                     return
